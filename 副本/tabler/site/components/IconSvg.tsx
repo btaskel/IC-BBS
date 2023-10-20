ce8af575301b31dbeb53412b2b765b1cbf8a52fa
btaskel <bt_asker@hotmail.com>
2023-10-20 21:15:07 +0800

    update

--- 副本/tabler/site/components/IconSvg.tsx
+++
@@ -1,6 +0,0 @@
-import icons from '@/data/icons.json';
-import clsx from 'clsx';
-
-export default function IconSvg({ svg, className }) {
-  return <span className={clsx('icon-svg', className)} dangerouslySetInnerHTML={{ __html: svg }} />;
-}
